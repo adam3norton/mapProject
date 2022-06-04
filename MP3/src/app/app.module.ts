221b3c0b7d1b8f5d5b34c292f4539a5bba034e6a
Adam Norton <adamnorton@Adams-MacBook-Pro.local>
2022-06-04 13:12:25 -0600

    Initialized MP3

--- MP3/src/app/app.module.ts
+++ MP3/src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AgmMapComponent } from './agm-map/agm-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AgmMapComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCCfzqOKwOKa5OpdbgrBgHYyzp9EheZ95k'
-    })
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
